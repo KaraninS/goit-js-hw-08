3ea081d2a0f62a0addadd36c733f38fe3edd9323
Karanin Serhii <36915523+KaraninS@users.noreply.github.com>
2022-05-17 10:30:37 +0300

    h8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+player.setCurrentTime(localStorage.getItem('time-after-pause'));
+
+player.on('timeupdate',  throttle(function(data) {
+    localStorage.setItem('time-after-pause', data.seconds)
+}),1000);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
 
+const formBox = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('input');
+const textareaMessage = document.querySelector('textarea');
+const KEY_LOCALSTORAGE = 'feedback-form-state';
+const textInMemory = JSON.parse(localStorage.getItem(KEY_LOCALSTORAGE));
+const inputObject = {};
+
+if (textInMemory) {
+  inputEmail.value = textInMemory.email;
+  textareaMessage.value = textInMemory.message;
+}
+
+formBox.addEventListener('input', throttle(addToLocal, 500));
+formBox.addEventListener('submit', sendForm);
+
+function addToLocal() {
+  inputObject[inputEmail.name] = inputEmail.value;
+  inputObject[textareaMessage.name] = textareaMessage.value;
+  localStorage.setItem(KEY_LOCALSTORAGE, JSON.stringify(inputObject));
+}
+
+function sendForm(event) {
+  event.preventDefault();
+  console.log(inputObject);
+  event.currentTarget.reset();
+  localStorage.clear();
+}
